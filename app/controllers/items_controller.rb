9988b6e5b58ccd02df57b7c222a5651212446f3f
panumai2000 <81949157+panumai2000@users.noreply.github.com>
2021-04-30 21:27:35 +0900

    Revert "商品情報編集機能の実装"
    This reverts commit 76606025b01163a2e59e6c39b98e1594344fcf4b.

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -23,19 +23,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
-    if @item.update(item_params)
-      redirect_to item_path
-    else
-      render :edit
-    end
-  end
-
-  def edit
-    @item = Item.find(params[:id])
-    unless @item.user_id == current_user.id
-      redirect_to root_path
-    end
   end
 
   private
